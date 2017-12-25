eedabe309c72d941a3af1b40b727970a6cb34f2e
Dhruv Bhardwaj <dhruv2180@gmail.com>
2017-12-25 00:16:28 -0500

    update the javadoc

--- tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -10,6 +10,12 @@ public class TicTacToe {
         board=new boolean[3][3];
     }
 
+    /*
+     * This method checks if move is valid or not.
+     * if valid it updates the board matrix otherwise
+     * throws a runtime exception.
+     */
+
     public void play(int x,int y){
 
         if(x<1 || x>3){
@@ -26,11 +32,17 @@ public class TicTacToe {
         }
     }
 
+    /*
+     * This method updates the spot on the board with the move.
+     */
+
     public void updateBoard(int x,int y){
         board[x-1][y-1]=true;
     }
 
-    //Funtion to
+    /*
+     * This method checks if the spot on the board is already occupied.
+     */
     public boolean checkBoardSpot(int x,int y){
         if(board[x-1][y-1]==true){
             return true;
