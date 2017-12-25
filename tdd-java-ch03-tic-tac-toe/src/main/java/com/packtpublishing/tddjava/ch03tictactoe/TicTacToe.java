d5d979112f3c17efd03a595f15508d268bd61839
Dhruv Bhardwaj <dhruv2180@gmail.com>
2017-12-25 02:31:29 -0500

    remaining requirements for chapter 3

--- tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -5,18 +5,19 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  */
 public class TicTacToe {
 
-    boolean board[][];
+    player board[][];
+    int numOfMoves=0;
 
-    enum player{
+    enum player {
         X,
         Y
     }
 
     private player nextPlayer;
 
-    public TicTacToe(){
-        board=new boolean[3][3];
-        nextPlayer=player.X;
+    public TicTacToe() {
+        board = new player[3][3];
+        nextPlayer = player.X;
 
     }
 
@@ -26,40 +27,43 @@ public class TicTacToe {
      * throws a runtime exception.
      */
 
-    public void play(int x,int y){
+    public String play(int x, int y) {
 
-        if(x<1 || x>3){
+        if (x < 1 || x > 3) {
             throw new RuntimeException("Invalid X");
         }
-        if(y<1 || y>3){
+        if (y < 1 || y > 3) {
             throw new RuntimeException("Invalid Y");
         }
 
-        if(!checkBoardSpot(x,y)){
-            updateBoard(x,y);
-        }else{
+        if (!checkBoardSpot(x, y)) {
+            updateBoard(x, y);
+        } else {
             throw new RuntimeException("Spot is already occupied");
         }
+
+        numOfMoves++;
+        return checkWinner();
     }
 
     /*
      * This method updates the spot on the board with the move.
      */
 
-    public void updateBoard(int x,int y){
-        board[x-1][y-1]=true;
-        if(nextPlayer.equals(player.X)){
-            nextPlayer=player.Y;
-        }else{
-            nextPlayer=player.X;
+    public void updateBoard(int x, int y) {
+        board[x - 1][y - 1] = nextPlayer;
+        if (nextPlayer.equals(player.X)) {
+            nextPlayer = player.Y;
+        } else {
+            nextPlayer = player.X;
         }
     }
 
     /*
      * This method checks if the spot on the board is already occupied.
      */
-    public boolean checkBoardSpot(int x,int y){
-        if(board[x-1][y-1]==true){
+    public boolean checkBoardSpot(int x, int y) {
+        if (board[x - 1][y - 1] != null) {
             return true;
         }
         return false;
@@ -69,7 +73,117 @@ public class TicTacToe {
      * This method return the next player that is expected to move
      */
 
-    public player getNextPlayer(){
+    public player getNextPlayer() {
         return nextPlayer;
+    }
+
+     /*
+     * This method checks if there is a winner
+     */
+
+    public String checkWinner() {
+
+
+        player winner = checkHorizontalWinner();
+        if (winner != null) {
+            return winner.toString() + "is winner";
+        }
+
+        winner = checkVerticalWinner();
+        if (winner != null) {
+            return winner.toString() + "is winner";
+        }
+        winner=checkDiagonalWinner();
+        if (winner != null) {
+            return winner.toString() + "is winner";
+        }
+
+        if(numOfMoves==9){
+            return "Game is a Draw";
+        }
+
+        return "No Winner";
+    }
+
+    /*
+     * Check Horizontal winner
+     */
+
+    public player checkHorizontalWinner() {
+
+        player first;
+        player second;
+        player third;
+
+        for (int i = 0; i < 3; i++) {
+            //constant x
+            first = board[i][0];
+            second = board[i][1];
+            third = board[i][2];
+
+            if (first == second && second == third) {
+                return first;
+            } else {
+                continue;
+            }
+        }
+
+        return null;
+    }
+    /*
+     * Check Vertical winner
+     */
+
+    public player checkVerticalWinner() {
+
+        player first;
+        player second;
+        player third;
+
+        for (int i = 0; i < 3; i++) {
+            //constant y
+            first = board[0][i];
+            second = board[1][i];
+            third = board[2][i];
+
+            if (first == second && second == third) {
+                return first;
+            } else {
+                continue;
+            }
+        }
+
+        return null;
+    }
+
+    /*
+     * Check Diagnoal winner
+     */
+
+    public player checkDiagonalWinner() {
+
+        player first;
+        player second;
+        player third;
+
+
+        first = board[0][0];
+        second = board[1][1];
+        third = board[2][2];
+
+        if (first == second && second == third) {
+            return first;
+        }
+
+        first = board[0][2];
+        second = board[1][1];
+        third = board[2][0];
+
+        if (first == second && second == third) {
+            return first;
+        }
+
+
+        return null;
     }
 }
--- tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -32,37 +32,104 @@ public class TicTacToeSpec {
     }
 
     @Test
-    public void whenSpotAlreadyOccupiedThenThrowRuntimeException(){
+    public void whenSpotAlreadyOccupiedThenThrowRuntimeException() {
 
-        ticTacToe.play(1,1);
+        ticTacToe.play(1, 1);
         exp.expect(RuntimeException.class);
-        ticTacToe.play(1,1);
+        ticTacToe.play(1, 1);
 
     }
 
     @Test
-    public void whenGameBeginsNextPlayerIsX(){
+    public void whenGameBeginsNextPlayerIsX() {
 
-        assertEquals(TicTacToe.player.X,ticTacToe.getNextPlayer());
-
-    }
-
-    @Test
-    public void whenXPlayedThenNextPlayerIsO(){
-
-        ticTacToe.play(1,1);
-        assertEquals(TicTacToe.player.Y,ticTacToe.getNextPlayer());
+        assertEquals(TicTacToe.player.X, ticTacToe.getNextPlayer());
 
     }
 
     @Test
-    public void whenOPlayedThenNextPlayerisX(){
+    public void whenXPlayedThenNextPlayerIsO() {
 
-        ticTacToe.play(1,1);
-        ticTacToe.play(2,2);
-        ticTacToe.play(1,2);
-        ticTacToe.play(2,1);
-        assertEquals(TicTacToe.player.X,ticTacToe.getNextPlayer());
+        ticTacToe.play(1, 1);
+        assertEquals(TicTacToe.player.Y, ticTacToe.getNextPlayer());
 
+    }
+
+    @Test
+    public void whenOPlayedThenNextPlayerisX() {
+
+        ticTacToe.play(1, 1);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 1);
+        assertEquals(TicTacToe.player.X, ticTacToe.getNextPlayer());
+
+    }
+
+    @Test
+    public void whenNoWinnerThenReturnNill() {
+        assertEquals("No Winner", ticTacToe.play(1, 1));
+    }
+
+    @Test
+    public void whenXMarkedAllHorizontalSpotsThenXisWinner() {
+
+        ticTacToe.play(1, 1);//x
+        ticTacToe.play(2, 1);//o
+        ticTacToe.play(1, 2);//x
+        ticTacToe.play(2, 2);//o
+        assertEquals(TicTacToe.player.X.toString() + "is winner", ticTacToe.play(1, 3));
+
+    }
+
+    @Test
+    public void whenXMarkedAllVerticalSpotsThenXisWinner() {
+
+        ticTacToe.play(1, 1);//x
+        ticTacToe.play(1, 2);//o
+        ticTacToe.play(2, 1);//x
+        ticTacToe.play(2, 2);//o
+        assertEquals(TicTacToe.player.X.toString() + "is winner", ticTacToe.play(3, 1));
+
+    }
+
+    @Test
+    public void whenOMarkedAllDiagonalThenOisWinner() {
+
+        ticTacToe.play(1, 2);//x
+        ticTacToe.play(1, 1);//o
+        ticTacToe.play(2, 1);//x
+        ticTacToe.play(2, 2);//o
+        ticTacToe.play(3, 1);//x
+        assertEquals(TicTacToe.player.Y.toString() + "is winner", ticTacToe.play(3, 3));
+
+
+    }
+
+    @Test
+    public void WhenOppositeDiagonalThenXIsWinner() {
+
+        ticTacToe.play(1, 3);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(2, 3);
+        assertEquals(TicTacToe.player.X.toString() + "is winner", ticTacToe.play(3, 1));
+
+
+    }
+
+    @Test
+
+    public void WhenAllBoxesAreFilledWithNoWinningConditionThenDraw() {
+
+        ticTacToe.play(1, 2); //X
+        ticTacToe.play(1, 1); //O
+        ticTacToe.play(2, 2); //X
+        ticTacToe.play(1, 3);//O
+        ticTacToe.play(2, 1); //X
+        ticTacToe.play(2, 3);//O
+        ticTacToe.play(3, 1); //X
+        ticTacToe.play(3, 2);//O
+        assertEquals("Game is a Draw", ticTacToe.play(3, 3));
     }
 }
