6fbc2458e7d6570b5d1b754e59e27886da2209f0
Dhruv Bhardwaj <dhruv2180@gmail.com>
2017-12-25 15:38:22 -0500

    Requirement 1 implementation

--- tdd-java-ch04-ship/src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ tdd-java-ch04-ship/src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,19 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    private Ship ship;
+    private Location location;
+    private Point point;
+
+    @BeforeMethod
+    public void beforeTest(){
+        point=new Point(1,1);
+        location=new Location(point,Direction.NORTH);
+        ship=new Ship(location);
+    }
+
+    public void whenShipsPointDirectionIsSetThenLocationIsSet(){
+        assertEquals(ship.getLocation(),location);
+    }
+
 }
