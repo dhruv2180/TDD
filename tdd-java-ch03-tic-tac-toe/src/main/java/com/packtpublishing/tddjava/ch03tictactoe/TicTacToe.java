a24e69b9f574e54212abcdb193aa6c032361360e
Dhruv Bhardwaj <dhruv2180@gmail.com>
2017-12-25 00:11:41 -0500

    update code with requirement no.1

--- tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -5,13 +5,36 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  */
 public class TicTacToe {
 
+    boolean board[][];
     public TicTacToe(){
-
+        board=new boolean[3][3];
     }
 
     public void play(int x,int y){
 
+        if(x<1 || x>3){
+            throw new RuntimeException("Invalid X");
+        }
+        if(y<1 || y>3){
+            throw new RuntimeException("Invalid Y");
+        }
 
+        if(!checkBoardSpot(x,y)){
+            updateBoard(x,y);
+        }else{
+            throw new RuntimeException("Spot is already occupied");
+        }
+    }
 
+    public void updateBoard(int x,int y){
+        board[x-1][y-1]=true;
+    }
+
+    //Funtion to
+    public boolean checkBoardSpot(int x,int y){
+        if(board[x-1][y-1]==true){
+            return true;
+        }
+        return false;
     }
 }
--- tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -8,17 +8,33 @@ import org.junit.rules.ExpectedException;
 public class TicTacToeSpec {
 
     @Rule
-    public ExpectedException exp=ExpectedException.none();
+    public ExpectedException exp = ExpectedException.none();
     private TicTacToe ticTacToe;
 
     @Before
-    public final void before(){
-        ticTacToe=new TicTacToe();
+    public final void before() {
+        ticTacToe = new TicTacToe();
     }
 
     @Test
-    public void whenXisOutsideThenThrowRuntimeException(){
+    public void whenXisOutsideThenThrowRuntimeException() {
         exp.expect(RuntimeException.class);
-        ticTacToe.play(5,2);
+        ticTacToe.play(5, 2);
 
-    }}
+    }
+
+    @Test
+    public void whenYisOutsideThenThrowRuntimeException() {
+        exp.expect(RuntimeException.class);
+        ticTacToe.play(2, 5);
+    }
+
+    @Test
+    public void whenSpotAlreadyOccupiedThenThrowRuntimeException(){
+
+        ticTacToe.play(1,1);
+        exp.expect(RuntimeException.class);
+        ticTacToe.play(1,1);
+
+    }
+}
